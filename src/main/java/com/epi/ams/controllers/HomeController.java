7e7aba14797c90dc10cb8a5246c17169a24c28c6
fedi1919 <fedi.hammami.stic@gmail.com>
2022-01-20 13:13:53 +0100

    add avec post

--- src/main/java/com/epi/ams/controllers/HomeController.java
+++ src/main/java/com/epi/ams/controllers/HomeController.java
@@ -3,12 +3,23 @@ package com.epi.ams.controllers;
 import com.epi.ams.entities.Candidat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
 
 @Controller
 class HomeController {
+
+    static ArrayList<Candidat> lc; // il faut definir lc  comme static pour pouvoir le manipuler dans le tableau static
+
+    static {   // tableau static
+        lc = new ArrayList<>();
+        Candidat c1 = new Candidat(1,"fedi","fediHammami@gmail.com","54422162");
+        Candidat c2 = new Candidat(2,"tayeb","tayebHammami@gmail.com","27520175");
+        Candidat c3 = new Candidat(3,"isra","israHammami@gmail.com","54422662");
+        lc.add(c1); lc.add(c2); lc.add(c3);
+    }
+
     @RequestMapping("/index")
     // @ResponseBody
     public String home(){
@@ -19,20 +30,34 @@ class HomeController {
     @RequestMapping("/candidats")
     public String listCandidats(Model m){  // Model permet de transferer les donnees vers le view(.html) avec la methode add attribute
 
-        Candidat c1 = new Candidat(1,"fedi","fediHammami@gmail.com","54422162");
+        /* Candidat c1 = new Candidat(1,"fedi","fediHammami@gmail.com","54422162");
         Candidat c2 = new Candidat(2,"tayeb","tayebHammami@gmail.com","27520175");
         Candidat c3 = new Candidat(3,"isra","israHammami@gmail.com","54422662");
         Candidat tab[] = new Candidat[3];
-        tab[0]= c1;tab[1]= c2;tab[2]= c3;
+        tab[0]= c1;tab[1]= c2;tab[2]= c3;*/
         String libelleFormation = "Spring boot Angulair";
         String formateur = "Med Amin";
+
         m.addAttribute("lf",libelleFormation);
         m.addAttribute("trainer",formateur);
-        m.addAttribute("tab",tab);
+        m.addAttribute("tab",lc);
         return "home/candidats";
     }
+
     @GetMapping("/add")
     public String addCandidate(){
         return "home/add";
+    }
+
+    @PostMapping("/add")
+    // @ResponseBody : pour afficher dans le html
+    public String saveCandidate(@RequestParam("id") int id,
+                                @RequestParam("nom") String nom,
+                                @RequestParam("email") String email,
+                                @RequestParam("tel") String tel){
+        Candidat temp = new Candidat(id,nom,email,tel);
+        lc.add(temp);
+        //return "infos : "+id+" "+nom+" "+email+" "+tel;
+        return "redirect:candidats";
     }
 }
